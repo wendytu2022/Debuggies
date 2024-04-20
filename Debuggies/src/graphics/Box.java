2497b9a928b652081e6d0440aa18400de50b679e
Brian Xie <brianxie1298@gmail.com>
2024-04-19 22:39:32 -0400

    Box

---
+++ Debuggies/src/graphics/Box.java
@@ -0,0 +1,27 @@
+package graphics;
+import java.util.ArrayList;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+
+public abstract class Box {
+//	centerX: x-axis center of the box
+//	centerY: y-axis center of the box
+//	width: width of the box
+//	height: height of the box
+	
+	protected float centerX, centerY, width, height;
+
+	public Box() {
+//		Setting Default Values
+//		Position
+		centerX = 0;
+		centerY = 0;		
+//		Size
+		width = 0;
+		height = 0;
+	}
+
+	
+	
+	
+}
