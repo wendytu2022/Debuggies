b8b5fb77c05fdcb0965cce71f5585bc4745affed
Shu-Ye Lin <67604195+shuyelin06@users.noreply.github.com>
2024-04-20 12:58:44 -0400

    StackOverFlow and SyntaxEnemy

--- Debuggies/src/objects/StackOverflowEnemy.java
+++ Debuggies/src/objects/StackOverflowEnemy.java
@@ -7,28 +7,54 @@ import objects.projectiles.Spike;
 import java.lang.Math;
 import java.util.concurrent.TimeUnit;
 
+import core.states.ExampleState;
 import geometry.Vector;
 
 public class StackOverflowEnemy extends Enemy {
+	private static final int numShots = 5;
+	private static final float ShotDelay = 0.25f;
+	
 	// shorter time between shots to shoot a ton of bullets
-	protected int timeBetweenShots = 1;
-	public StackOverflowEnemy(Team team, int health, EnemyErrorType enemyErrorType) {
-		super(team, health, enemyErrorType);
+	protected float timeBetweenShots;
+	
+	public StackOverflowEnemy() {
+		super(Team.Enemy, 300, EnemyErrorType.StackOverflow);
+		
+		timeBetweenShots = ShotDelay;
 	}
 	
 	// shoot a booty ton of bullets
 	@Override
 	public void update(float deltaTime) {
-		if (deltaTime > timeBetweenShots) {
-			int max = 10;
-			int min = 1;
-			int range = max - min + 1;
-			float randX = (float)(Math.random() * range) + min;
-			float randY = (float)(Math.random() * range) + min;
+		Entity player = ExampleState.player;
+		
+		timeBetweenShots -= deltaTime;
+		
+		// Set velocity to always be moving towards player
+		Vector direction = position.lookAt(player.getPosition()).normalize();
+		
+		final float velo = 1.5f;
+		velocity.assign(direction);
+		velocity.scaleInplace(velo);
+		
+		if (timeBetweenShots < 0) {
+			timeBetweenShots = ShotDelay;
 			
-			Spike bullet = new Spike(this, new Vector(randX, randY));
+			final int max = 10;
+			final int min = -10;
+			final int range = max - min + 1;
+			
+			for (int i = 0; i < numShots; i++) {
+				float randX = (float)(Math.random() * range) + min;
+				float randY = (float)(Math.random() * range) + min;
+
+				new Spike(this, new Vector(randX, randY));
+			}
+			
+			
 		}
 		
 		// shoot a booty ton of bullets
 	}
+
 }
