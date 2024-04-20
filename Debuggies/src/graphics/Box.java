0eec8312c9ae174b84539ab0393b9d2b80f12e13
Brian Xie <brianxie1298@gmail.com>
2024-04-19 22:45:15 -0400

    Update Box.java

--- Debuggies/src/graphics/Box.java
+++ Debuggies/src/graphics/Box.java
@@ -11,17 +11,55 @@ public abstract class Box {
 	
 	protected float centerX, centerY, width, height;
 
+//	Abstract method for subclasses that require interaction
+	protected abstract void mouseClick(float mouseX, float mouseY);
+	
+//	Values should be updated by subclasses
 	public Box() {
 //		Setting Default Values
 //		Position
-		centerX = 0;
-		centerY = 0;		
+		centerX = -1;
+		centerY = -1;		
 //		Size
-		width = 0;
-		height = 0;
+		width = -1;
+		height = -1;
+	}
+	
+//	Initializing Methods
+	public void initialize() {}
+	public void update() {}
+	
+//	
+	
+//	Setting Functions to update values
+	public Box setX(float x) {
+		centerX = x; 
+		return this;
+	}
+	
+	public Box setY(float y) {
+		centerY = y;
+		return this;
+	}
+	
+	public Box setHeight(float h) {
+		height = h;
+		return this;
+	}
+	
+	public Box setWidth(float w) {
+		width = w;
+		return this;
+	}
+	
+	public void draw(Graphics d) {
+//		Setting Color of Box
+		d.setColor(Color.white);
+		
+//		Parameters: X, Y, Width, Height
+		d.fillRect(0, 0, 10, 10);;
 	}
 
-	
 	
 	
 }
