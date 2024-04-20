850fae6ae31c47442ab229b35f3a6df9bde4c03b
Anna Dai <anna.dai916@gmail.com>
2024-04-20 12:43:32 -0400

    Added screenshots of code

--- bugged_code/Bug.java
+++ bugged_code/Bug.java
@@ -18,64 +18,70 @@ public class Bug {
 
 	// NullPointerException
 
-	// Method that tests if a Linked List is ordered
-	public boolean isIncreasingOrder(Node head) {
-		boolean result = true;
+// Method that tests if a Linked List is ordered
+public boolean isIncreasingOrder(Node head) {
+	boolean result = true;
 
-		Node curr = head;
-		while (result && curr != null) {
-			if (curr.data > curr.next.data)
-				result = false;
-			curr = curr.next;
-		}
-
-		return result;
+	Node curr = head;
+	while (result && curr != null) {
+		if (curr.data > curr.next.data)
+			result = false;
+		curr = curr.next;
 	}
+
+	return result;
+}
 	
 	// StackOverflowError
 	
-	public int addNumber(int n) {
-		n += 2;
-		return addNumber(n + 2);
-	}
+public int addNumber(int n) {
+	n += 2;
+	return addNumber(n + 2);
+}
 	
 	 
 	public static void main(String[] args) {
 		// ArrayIndexOutOfBoundsException\
-		String[] arr = {"We", "love", "bitcamp"};
-		System.out.println(arr[3]);
 		
-		// ConcurrentModificationException
-		ArrayList<String> list = new ArrayList<>();
-		list.add("We");
-		list.add("love");
-		list.add("bitcamp!");
+
 		
-		for (String s : list) {
-			System.out.println(s);
-			list.remove(s);
-		}
+// ConcurrentModificationException
+ArrayList<String> list = new ArrayList<>();
+list.add("We");
+list.add("love");
+list.add("bitcamp!");
 		
-		// Syntax Error
-		System.out.println("We love bitcamp!");
-		System.out.prinln("Because bitcamp is so much fun");
-		System.out.println("Live love bitcamp");
+for (String s : list) {
+	System.out.println(s);
+	list.remove(s);
+}
 		
-		for (int i = 0; i < 2024; i++) 
-			System.out.println("Let's get hacking!")							;
+// Syntax Error
+		
+System.out.println("We love bitcamp!");
+System.out.println("Because bitcamp is so much fun!");
+System.out.println("Live love bitcamp");
+		
+for (int i = 0; i < 2024; i++) 
+	System.out.println("Let's get hacking!")							;
 			
-		
+String[] arr = {"We", "love", "bitcamp"};
+System.out.println(arr[3]);
 			
 		// Uninitialized variable
-		int x = 20;
-		int y = 24;
+		int z = 20;
 		
-		if (x + y + z > 0)
-			System.out.println("Bitcamp is the best");
-		else
-			System.out.println("Bitcamp is still the best");
 		
-	}
+int x = 20;
+int y = 24;
+int z;
+	
+if (x + y + z > 0)
+	System.out.println("Bitcamp is the best!");
+else
+	System.out.println("Bitcamp is still the best!");
+		
+}
 
 	
 }
